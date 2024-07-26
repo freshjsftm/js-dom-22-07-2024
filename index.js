5ed99f18913d6b3911aa624fc6aabd0817856367
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-26 17:42:11 +0300

    Add Gallery & HTMLCollection

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-console.dir(document);
-const h1 = document.getElementById('main-title');
-h1.className = 'heading';
-h1.textContent = 'hi from javascript!';
-h1.title = 'title for main title';
-console.dir(h1);
-const word = 'image';
-const img = document.getElementById(word);
-console.dir(img);
-console.log(img);
-img.src =
-  'https://www.earth.com/_next/image/?url=https%3A%2F%2Fcff2.earth.com%2Fuploads%2F2023%2F06%2F16080040%2Fsea-turtle_3medium-960x640.jpg&w=3840&q=75';
-img.alt = 'turtle';
-img.width = 400;
-img.height = 400;
-img.style.objectFit = 'cover';
+const bigImg = document.getElementById('big');
 
-// забрати кнопку
-// для кнопки зробити розмір - 200px х 70px
-// замінити текст на click me, please
-const btn = document.getElementById('btn');
-console.dir(btn);
-btn.style.height = '70px';
-btn.style.width = '200px';
-btn.textContent += ', please';
+//HTMLCollection
+const smallImgs = document.getElementsByClassName('small');
+
+//перебираємо колекцію  forof
+// for (const element of HTMLCollection) {
+//   element
+// }
+
+for (const smallImg of smallImgs) {
+  smallImg.addEventListener('click', () => {
+    bigImg.src = smallImg.src;
+  });
+}
