0b3da2c4a6815c64328ca907a8443e67d500f7ba
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-08-14 17:43:02 +0300

    Add classList

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-const h2Block = document.querySelector('#block > h2');
-//console.dir(h2Block);
-h2Block.style.color = 'white';
+const buttons = document.querySelectorAll('button');
 
-//console.dir(document.getElementsByClassName('btn'));
+// classList
+// mouseenter      mouseover
+// mouseleave      mouseout
 
-const btnsBlock = document.querySelectorAll('#block > .btn');
-
-btnsBlock.forEach((element) => {
-  element.style.backgroundColor = 'yellow';
-});
-for (const element of btnsBlock) {
-  element.style.fontSize = '16px';
-}
-
-/*
-querySelectorAll зібрати усі li і додати на них клік і по кліку змінювати текст всередині li на 'WoW'
-*/
-
-const lis = document.querySelectorAll('#block>ol>li');
-console.dir(lis);
-lis.forEach(li => {
-  li.addEventListener('click', ()=>{
-    li.textContent = 'WoW';
+for (const btn of buttons) {
+  // console.dir(btn.classList);
+  btn.classList.add('btn');
+  btn.addEventListener('mouseover', ()=>{
+    btn.classList.add('active')
   })
-});
-
-/*
-першому пункту списку зробити фон рожевим pink
-останньому пункту списку зробити фон рожевим pink
- */
-
-
-document.querySelector('#block > ol > li:first-child').style.backgroundColor = 'pink';
-
-
-document.querySelector('#block > ol > li:last-child').style.backgroundColor = 'pink';
+  btn.addEventListener('mouseout', ()=>{
+    btn.classList.remove('active')
+  })
+}
