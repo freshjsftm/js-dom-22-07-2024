9daea839213bd97ca83141b7615cdac8266c235a
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-08-14 17:53:56 +0300

    Add toggle

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const buttons = document.querySelectorAll('button');
 for (const btn of buttons) {
   // console.dir(btn.classList);
   btn.classList.add('btn');
-  btn.addEventListener('mouseover', ()=>{
-    btn.classList.add('active')
-  })
-  btn.addEventListener('mouseout', ()=>{
-    btn.classList.remove('active')
-  })
+  btn.addEventListener('mouseover', () => {
+    btn.classList.add('active');
+  });
+  btn.addEventListener('mouseout', () => {
+    btn.classList.remove('active');
+  });
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('shadow');
+  });
 }
