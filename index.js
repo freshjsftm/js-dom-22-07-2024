8dc5bcd5733bb7ad4ab282114305cad8d51637b2
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-08-19 18:17:11 +0300

    Add event

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const buttons = document.querySelectorAll('button');
-
-// classList
-// mouseenter      mouseover
-// mouseleave      mouseout
-
-for (const btn of buttons) {
-  // console.dir(btn.classList);
-  btn.classList.add('btn');
-  btn.addEventListener('mouseover', () => {
-    btn.classList.add('active');
-  });
-  btn.addEventListener('mouseout', () => {
-    btn.classList.remove('active');
-  });
-  btn.addEventListener('click', () => {
-    btn.classList.toggle('shadow');
-  });
-}
+const wrapper = document.getElementById('wrapper');
+const btn = document.getElementById('btn');
+wrapper.addEventListener('click', (event) => {
+  console.log('click wrapper')
+  // console.group();
+  // console.log(event);
+  // console.log(event.target); // елемент по якому клікнули
+  // console.log(event.currentTarget); //елемент чия функція виконується
+  // console.log(event.clientX, event.clientY);
+  // console.log(event.offsetX, event.offsetY);
+  // console.groupEnd();
+});
+// wrapper.click() // сгенерували штучно клік (подію)
+btn.addEventListener('click', ()=>{
+  console.log('click btn')
+})
