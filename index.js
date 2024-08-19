38c1ced2191690fefab6020b437bee78dfeef688
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-08-19 18:29:12 +0300

    Add stopPropagation

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const wrapper = document.getElementById('wrapper');
 const btn = document.getElementById('btn');
+
 wrapper.addEventListener('click', (event) => {
-  console.log('click wrapper')
-  // console.group();
-  // console.log(event);
-  // console.log(event.target); // елемент по якому клікнули
-  // console.log(event.currentTarget); //елемент чия функція виконується
-  // console.log(event.clientX, event.clientY);
-  // console.log(event.offsetX, event.offsetY);
-  // console.groupEnd();
+  console.log('click wrapper');
+  event.target.style.color = 'red';
 });
-// wrapper.click() // сгенерували штучно клік (подію)
-btn.addEventListener('click', ()=>{
-  console.log('click btn')
-})
+
+btn.addEventListener('click', (event) => {
+  event.stopPropagation(); // виконує лише поточну функцію
+  console.log('click btn');
+});
