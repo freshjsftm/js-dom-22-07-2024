dde0c97bd294fe805039e658d7c593a75d879555
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-08-21 17:32:31 +0300

    Add move with key

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const wrapper = document.getElementById('wrapper');
-const btn = document.getElementById('btn');
+const block = document.getElementById('block');
+// console.dir(block);
+// wrapper.addEventListener('mousemove', (event)=>{
+//   block.style.left = event.clientX + 'px';
+//   block.style.top = event.clientY + 'px';
+// })
 
-wrapper.addEventListener('click', (event) => {
-  console.log('click wrapper');
-  event.target.style.color = 'red';
-});
+let x = 0;
+let y = 0;
+const step = 5;
 
-btn.addEventListener('click', (event) => {
-  event.stopPropagation(); // виконує лише поточну функцію
-  console.log('click btn');
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') {
+    x-=step;
+  }
+  if (event.key === 'ArrowRight') {
+    x+=step;
+  }
+  if (event.key === 'ArrowUp') {
+    y-=step;
+  }
+  if (event.key === 'ArrowDown') {
+    y+=step;
+  }
+  block.style.left = x + 'px';
+  block.style.top = y + 'px';
 });
