467bdde7224f6705c0e6b8c486424895a40925a5
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-08-26 17:08:02 +0300

    Add createCard

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const root = document.getElementById('root');
-const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink"];
+const products = [
+  { name: 'Яблука', category: 'Фрукти', price: 40 },
+  { name: 'Хліб', category: 'Випічка', price: 25 },
+  { name: 'Молоко', category: 'Молочні продукти', price: 30 },
+  { name: 'Курячі грудки', category: "М'ясо", price: 120 },
+  { name: 'Картопля', category: 'Овочі', price: 20 },
+  { name: 'Помідори', category: 'Овочі', price: 50 },
+  { name: 'Сир', category: 'Молочні продукти', price: 80 },
+  { name: 'Яйця', category: 'Молочні продукти', price: 35 },
+  { name: 'Макарони', category: 'Бакалія', price: 45 },
+  { name: 'Чай', category: 'Напої', price: 60 },
+  { name: 'Шоколад', category: 'Солодощі', price: 70 },
+  { name: 'Сік апельсиновий', category: 'Напої', price: 55 },
+  { name: 'Банани', category: 'Фрукти', price: 65 },
+  { name: 'Рис', category: 'Бакалія', price: 50 },
+  { name: 'Огірки', category: 'Овочі', price: 30 },
+];
 
-const btns = colors.map((color)=>{
+const section = document.createElement('section');
+section.className = 'shop';
+const cards = products.map((product) => createCard(product));
+section.append(...cards);
+root.append(section);
+
+function createCard(product) {
+  const { name, category, price } = product;
+  const article = document.createElement('article');
+  article.classList.add('product');
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('product-name');
+  h2.textContent = name;
+
+  const h3 = document.createElement('h3');
+  h3.classList.add('product-category');
+  h3.textContent = category;
+
+  const p = document.createElement('p');
+  p.classList.add('product-price');
+  p.textContent = price+' uah';
+
   const btn = document.createElement('button');
-  btn.textContent = color;
-  btn.style.backgroundColor = color;
-  return btn;
-})
+  btn.classList.add('product-addtocart');
+  btn.textContent = 'add to cart';
 
-root.append(...btns);
+  article.append(h2, h3, p, btn);
+  return article;
+}
