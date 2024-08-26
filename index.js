e452aaf4f2c06cc034dc55c4b15ac52d77818914
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-08-26 18:43:26 +0300

    Add setInterval

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 const root = document.getElementById('root');
 
-const btnSubscribe = document.createElement('button');
-btnSubscribe.textContent = 'Subscribe now!';
-root.append(btnSubscribe);
-
-// відтермінований запуск
-// setTimeout(функція, часВМіліСекундах)
-
-const idTimeout = setTimeout(
-  () => {
-  const removeSubscribe = () => {
-    document.querySelector('.subscribe').remove();
-  };
-  const subscribe = document.createElement('div');
-  subscribe.classList.add('subscribe');
-  const close = document.createElement('span');
-  close.textContent = 'X';
-  close.addEventListener('click', removeSubscribe);
-  const h2 = document.createElement('h2');
-  h2.textContent = 'Subscribe to our email newsletter';
-  const form = document.createElement('form');
-  form.className = 'subscribe-form';
-  const input = document.createElement('input');
-  const button = document.createElement('button');
-  button.textContent = 'Subscribe';
-  button.addEventListener('click', removeSubscribe);
-  subscribe.append(close, h2, form);
-  form.append(input, button);
-  root.append(subscribe);
-}, 5000);
-
-btnSubscribe.addEventListener('click', ()=>{
-  clearTimeout(idTimeout)
-})
-
 // викликає код з певним інтервалом
-// setInterval
+// setInterval(function, timeInMs)
+
+let count = 0;
+const idInterval = setInterval(() => {
+  console.log(count);
+  count++;
+  if (count > 10) {
+    clearInterval(idInterval);
+  }
+}, 1000);
+
