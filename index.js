14d8a7f8df9118a3cd6eb293e79bb0e6175e510f
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-02 17:13:25 +0300

    Add form

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-const root = document.getElementById('root');
+// const form1v = document.getElementById('form');
 
-// викликає код з певним інтервалом
-// setInterval(function, timeInMs)
+// спосіб дістати форму без ідентифікатора
+const [form] = document.forms;//HTMLCollection
+const login = form.elements.login;
+const password = form.elements.password;
+const submitBtn = document.querySelector('[type="submit"]');
+submitBtn.disabled = true;
 
-let count = 0;
-const idInterval = setInterval(() => {
-  console.log(count);
-  count++;
-  if (count > 10) {
-    clearInterval(idInterval);
+const loginPattern = /^[a-z0-9_-]{5,15}$/;
+const passwordPattern =
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,32}$/;
+// console.dir(form)
+// gr3at@3wdsG
+
+form.addEventListener('submit', (event) => {
+  //відміняємо обробку події за замовчуванням
+  event.preventDefault();
+  const login = form.elements.login.value.trim();
+  const password = form.elements.password.value.trim();
+  if (loginPattern.test(login) && passwordPattern.test(password)) {
+    form.submit();
+    alert('data send, thanks!');
+    return;
   }
-}, 1000);
+  alert('enter right login or password');
+});
 
+// login.addEventListener('focus', () => {
+//   login.style.backgroundColor = '#00ff0029';
+// });
+
+// login.addEventListener('blur', () => {
+//   const loginValue = login.value.trim();
+//   if (loginValue === '') {
+//     login.style.backgroundColor = 'red';
+//   } 
+// });
+
+// login.addEventListener('change', () => {
+//   login.style.backgroundColor = 'blue';
+// });
+
+// login.addEventListener('input', () => {
+//   const cirilic = /[А-Яа-я]+/;
+//   const loginValue = login.value.trim();
+//   if(cirilic.test(loginValue)){
+//     alert('ви пишите кирилицею!!!')
+//     login.style.fontSize = '30px';
+//   }else{
+//     login.style.fontSize = '';
+//   }
+//   login.style.backgroundColor = 'yellow';
+// });
