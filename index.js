d58d041adb7a883c4dfab97aa84d4e499a35f5e4
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-06 17:04:45 +0300

    Add form

--- index.js
+++ index.js
@@ -1,17 +1,84 @@
 // const form1v = document.getElementById('form');
 
 // спосіб дістати форму без ідентифікатора
-const [form] = document.forms;//HTMLCollection
+const [form] = document.forms; //HTMLCollection
 const login = form.elements.login;
+login.focus();
 const password = form.elements.password;
+const message = form.elements.message;
+const fruits = form.elements.fruits;
+const radioElements = form.elements.drink;
+const select = form.elements.town;
+const selectDep = form.elements.department;
+selectDep.disabled = true;
 const submitBtn = document.querySelector('[type="submit"]');
 submitBtn.disabled = true;
+let amountInput = 0;
 
 const loginPattern = /^[a-z0-9_-]{5,15}$/;
 const passwordPattern =
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,32}$/;
 // console.dir(form)
 // gr3at@3wdsG
+//textarea 
+//keydawn keyup keypress copy select paste cut 
+message.addEventListener('copy', (event)=>{
+  console.log(event.target.value)
+})
+
+const arrayFruits = [];
+fruits.forEach((fruct) => {
+  fruct.addEventListener('change', (event) => {
+    // console.log(event.target.checked);
+    // console.log(event.target.value);
+    arrayFruits.push(event.target.value);
+    console.log(arrayFruits);
+  });
+});
+
+select.addEventListener('change', () => {
+  if (select.value !== 'null') {
+    console.log(select.value);
+    selectDep.disabled = false;
+    selectDep.focus();
+  } else {
+    selectDep.disabled = true;
+  }
+});
+selectDep.addEventListener('change', () => {
+  if (selectDep.value !== 'null') {
+    console.log(selectDep.value);
+  }
+});
+
+radioElements.forEach((radio) => {
+  radio.addEventListener('change', (event) => {
+    console.dir(event.target.checked);
+    console.dir(event.target.value);
+    radioElements.forEach((elem) => {
+      if (elem !== event.target) {
+        elem.disabled = true;
+      }
+    });
+  });
+});
+
+login.addEventListener('change', () => {
+  if (loginPattern.test(login.value.trim())) {
+    amountInput++;
+  }
+  if (amountInput >= 2) {
+    submitBtn.disabled = false;
+  }
+});
+password.addEventListener('change', () => {
+  if (passwordPattern.test(password.value.trim())) {
+    amountInput++;
+  }
+  if (amountInput >= 2) {
+    submitBtn.disabled = false;
+  }
+});
 
 form.addEventListener('submit', (event) => {
   //відміняємо обробку події за замовчуванням
@@ -34,7 +101,7 @@ form.addEventListener('submit', (event) => {
 //   const loginValue = login.value.trim();
 //   if (loginValue === '') {
 //     login.style.backgroundColor = 'red';
-//   } 
+//   }
 // });
 
 // login.addEventListener('change', () => {
